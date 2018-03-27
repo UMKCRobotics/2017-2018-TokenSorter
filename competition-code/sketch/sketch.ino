a589581cfd9d6f6dae8b39f68ea02a9ad78c71f1
kosinkadink <kosinkadink1@gmail.com>
2018-03-27 16:27:00 -0500

    added proper pinout for wiring, about to test on robot

--- competition-code/sketch/sketch.ino
+++ competition-code/sketch/sketch.ino
@@ -1,4 +1,4 @@
-//#include "ScrapController.h"
+#include "ScrapController.h"
 #include "LineIntersection.h"
 //#include "TunnelRobot.h"
 //#include "Navigation.h"
@@ -9,11 +9,30 @@
 
 
 // define necessary constants here (pins)
-#define MIDDLE_IR_PIN A0
+
+// SENSOR CONSTANTS
+#define MIDDLE_IR_PIN A6
+// ARM CONSTANTS
+#define EM_RELAY 21
+#define ARM_LINEAR 23
+#define ARM_SERVO 22
+// MOTOR CONSTANTS
+#define PWM_LEFT 10
+#define PWM_RIGHT 9
+#define DIR_LEFT 12
+#define DIR_RIGHT 11
+// ENCODER CONSTANTS
+#define ENC_INT_LEFT 4
+#define ENC_INT_RIGHT 2
+#define ENC_CHECK_LEFT 5 
+#define ENC_CHECK_RIGHT 3 
 
 // define Scrap components here (motors, encoders, motor speed controls, etc)
+ScrapMotorSinglePin leftMotor = ScrapMotorSinglePin(DIR_LEFT,PWM_LEFT);
+ScrapMotorSinglePin rightMotor = ScrapMotorSinglePin(DIR_RIGHT,PWM_RIGHT);
 
-
+ScrapEncoder leftEncoder = ScrapEncoder(ENC_INT_LEFT,ENC_CHECK_LEFT);
+ScrapEncoder rightEncoder = ScrapEncoder(ENC_INT_RIGHT,ENC_CHECK_RIGHT);
 // define Movement component
 
 
@@ -32,6 +51,13 @@ void setup() {
 	Serial.begin(9600);
 	Serial.println("serial started");
 	line = new LineIntersection(MIDDLE_IR_PIN);
+}
+
+
+void initializePins() {
+	pinMode(EM_RELAY,OUTPUT);
+	pinMode(ARM_LINEAR,OUTPUT);
+	pinMode(ARM_SERVO,OUTPUT);
 }
 
 
